4ab7e5480d3784353ce74bab78ec64056b6a45bb
udaytopper <uday181122@gmail.com>
2026-08-23 11:12:39 +0530

    feat: add structured application error type

---
+++ src/utils/app-error.ts
@@ -0,0 +1,5 @@
+export class AppError extends Error {
+  constructor(public status: number, public code: string, message: string, public details: Record<string, unknown> = {}) {
+    super(message);
+  }
+}
